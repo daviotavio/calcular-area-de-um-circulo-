f36cd9cb44befc826c976b236e8ad0c02f7ec857
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-04 22:16:54 -0300

    Add files via upload

---
+++ Area de um circulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Area_de_um_circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int raio;
+
+            Console.Write("Digite o raio: ");
+            raio = int.Parse(Console.ReadLine());
+
+            Console.Write("a área é: " + Math.PI * raio * raio);
+
+            Console.ReadKey();
+        }
+    }
+}
